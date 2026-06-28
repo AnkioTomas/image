1857c5d4411f5fa2e2ad18b0659547feffc8ac39
ankio <ankio@ankio.net>
2026-06-28 12:34:20 +0800

    :sparkles: (core): 新增项目配置文件

--- src/config.php
+++ src/config.php
@@ -1,12 +1,26 @@
 <?php
-return [
-    'debug'=>true,//当前是否为调试模式
-    'timezone'=>'Asia/Shanghai',//时区
-    'default_route'=>true,//启用默认路由，nova默认根据url自动解析到AnyModule/AnyController/AnyMethod方法，如果设置为false，则需要手动配置路由
-    'domain'=>[
-        '0.0.0.0',//允许访问的域名
-    ],
-    'version'=>'1.0.0',//版本号
-    'ip' => "127.0.0.1",
-    'port' => 10211
-];
+return array (
+  'debug' => true,
+  'timezone' => 'Asia/Shanghai',
+  'default_route' => true,
+  'domain' => 
+  array (
+    0 => '0.0.0.0',
+  ),
+  'version' => '1.0.0',
+  'ip' => '127.0.0.1',
+  'port' => 10211,
+  'framework_start' => 
+  array (
+    0 => 'nova\\plugin\\login\\LoginManager',
+    1 => 'nova\\plugin\\tpl\\Handler',
+    2 => 'nova\\plugin\\webdav\\WebdavManager',
+    3 => 'nova\\plugin\\tpl\\Handler',
+    4 => 'nova\\plugin\\login\\LoginManager',
+    5 => 'nova\\plugin\\tpl\\Handler',
+    6 => 'nova\\plugin\\webdav\\WebdavManager',
+    7 => 'nova\\plugin\\tpl\\Handler',
+    8 => 'nova\\plugin\\login\\LoginManager',
+    9 => 'nova\\plugin\\tpl\\Handler',
+  ),
+);
